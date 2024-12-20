3dc121ce7bc93c36bc636955e8f098b371d1d9c7
khangIT <dvkhang2100321@student.ctuet.edu.vn>
2024-12-20 19:19:25 +0700

    tailwindcss + redux + redux-persist

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias illo
+        unde excepturi quis, hic officiis debitis sed voluptate quo enim ducimus
+        qui doloribus in sequi! Quisquam quod dolore quae adipisci.
+      </h1>
+    </>
+  );
+}
+
+export default App;
