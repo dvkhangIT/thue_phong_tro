4fde6dc2fee124380ecfc21ab6e2928e8da7bad7
Khang Duong Van <khangduong.dev@gmail.com>
2025-01-02 17:49:28 +0700

    Coding base server nodejs

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+require("dotenv").config();
+import cors from "cors";
+import initRoutes from "./src/routes";
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+app.use("/", (req, res) => {
+  res.send("server on...");
+});
+const port = process.env.PORT || 8888;
+const listener = app.listen(port, () => {
+  console.log(`server is running on the port ${listener.address().port}`);
+});
