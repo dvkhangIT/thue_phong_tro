235923385c1b7163720291f089c66b3c25216f27
Khang Duong Van <khangduong.dev@gmail.com>
2025-01-09 07:27:30 +0700

    onClick - login - register

--- client/src/containers/public/Header.jsx
+++ client/src/containers/public/Header.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/logowithoutbg.png";
 import { Button } from "../../components";
 import icons from "../../ultils/icons";
@@ -7,30 +7,32 @@ import { path } from "../../ultils/constan";
 const { AiOutlinePlusCircle } = icons;
 const Header = () => {
   const navigate = useNavigate();
-  const goLogin = useCallback(() => {
-    navigate(path.LOGIN);
+  const goLogin = useCallback((flag) => {
+    navigate(path.LOGIN, { state: { flag } });
   }, []);
   return (
     <div className="w-1100">
       <div className="flex items-center justify-between">
-        <img
-          src={logo}
-          alt="logo"
-          className="w-[240px] h-[70px] object-contain"
-        />
+        <Link to={"/"}>
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[240px] h-[70px] object-contain"
+          />
+        </Link>
         <div className="flex items-center gap-1">
           <span>Phongtro123.com xin chào!</span>
           <Button
             text={"Đăng nhập"}
             textColor="text-white"
             bgColor="bg-[#3961fb]"
-            onClick={goLogin}
+            onClick={() => goLogin(false)}
           ></Button>
           <Button
             text={"Đăng ký"}
             textColor="text-white"
             bgColor="bg-[#3961fb]"
-            onClick={goLogin}
+            onClick={() => goLogin(true)}
           ></Button>
           <Button
             text={"Đăng tin mới"}
--- client/src/containers/public/Login.jsx
+++ client/src/containers/public/Login.jsx
@@ -1,26 +1,56 @@
+import { useEffect, useState } from "react";
 import { Button, InputForm } from "../../components";
+import { useLocation } from "react-router-dom";
 
 const Login = () => {
+  const location = useLocation();
+  const [isRegister, setIsRegister] = useState(location.state?.flag);
+  console.log(location.state);
+  useEffect(() => {
+    setIsRegister(location.state?.flag);
+  }, [location.state?.flag]);
   return (
-    <div className="bg-white w-[600px] p-[30px] pb-[100px] rounded-sm shadow-sm">
-      <h3 className="text-2xl font-semibold">Đăng nhập</h3>
+    <div className="bg-white w-[600px] p-[30px] pb-[100px] rounded-sm shadow-sm mt-5">
+      <h3 className="text-2xl font-semibold">
+        {isRegister ? "Đăng ký tài khoản" : "Đăng nhập"}
+      </h3>
       <div className="flex flex-col w-full gap-5">
+        {isRegister && <InputForm label={"họ tên"}></InputForm>}
         <InputForm label={"số điện thoại"}></InputForm>
         <InputForm label={"mật khẩu"}></InputForm>
         <Button
           bgColor={"bg-secondary1"}
           textColor={"text-white"}
-          text={"Đăng nhập"}
+          text={isRegister ? "Đăng ký tài khoản" : "Đăng nhập"}
           fullwidth
         ></Button>
       </div>
       <div className="flex items-center justify-between cursor-pointer mt-7">
-        <small className="text-blue-700 hover:text-orange-700">
-          Bạn quên mật khẩu?
-        </small>
-        <small className="text-blue-700 cursor-pointer hover:text-orange-700">
-          Tạo tài khoản mới
-        </small>
+        {isRegister ? (
+          <small>
+            Bạn đã có tài khoản?
+            <span
+              onClick={() => setIsRegister(false)}
+              className="text-blue-500 hover:underline"
+            >
+              Đăng nhập ngay
+            </span>
+          </small>
+        ) : (
+          <>
+            <small className="text-blue-700 cursor-pointer hover:text-orange-700">
+              Bạn quên mật khẩu?
+            </small>
+            <small
+              onClick={() => {
+                setIsRegister(true);
+              }}
+              className="text-blue-700 cursor-pointer hover:text-orange-700"
+            >
+              Tạo tài khoản mới
+            </small>
+          </>
+        )}
       </div>
     </div>
   );
